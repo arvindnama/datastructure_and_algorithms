7b78e48ea76c885067024ce6f2b51adb0b74e11b
Arvind Nama <arvindnama@bluemount-macbook.local>
2024-05-06 00:12:17 -0700

    [BinaryTrees] height & insertion

---
+++ trees/binary-trees/helpers/node.ts
@@ -0,0 +1,41 @@
+
+export interface TreeNode<T> {
+  value: T;
+  left?: Nullable<TreeNode<T>>
+  right?: Nullable<TreeNode<T>>
+}
+
+export const createTreeNode = <T>(value?: T): Nullable<TreeNode<T>> => (value ? {value}: null)
+
+export function createTree<T>(values: T[]): TreeNode<T> {
+  const root: TreeNode<T> = createTreeNode(values.shift()) as TreeNode<T>;
+  const queue = [];
+  queue.push(root);
+  while(values.length) {
+    const nextVal1 = values.shift();
+    const nextVal2 = values.shift();
+    const node = queue.shift() as TreeNode<T>;
+    node.left = createTreeNode(nextVal1);
+    node.right = createTreeNode(nextVal2);
+    queue.push(node.left);
+    queue.push(node.right);
+
+  }
+  return root;
+}
+
+
+export function printTree<T>(root?: Nullable<TreeNode<T>>) {
+ const order = [];
+ const queue = [root];
+
+ while(queue.length) {
+  const node = queue.shift() as TreeNode<T>;
+  order.push(node?.value ?? null);
+  if(node){
+    queue.push(node.left);
+    queue.push(node.right);
+  }
+ }
+ console.log(order)
+}
---
+++ trees/binary-trees/insertion.ts
@@ -0,0 +1,32 @@
+/**
+ * Given a binary tree and a key, insert the key into the binary tree at the first position available in level order
+ */
+
+import { TreeNode, createTree, createTreeNode, printTree } from "./helpers/node";
+
+function insert<T>(root: TreeNode<T>, key: T): TreeNode<T> {
+
+  const queue = [root];
+  while(queue.length){
+    const node = queue.shift() as TreeNode<T>;
+    if(!node.left) {
+      node.left = createTreeNode(key);
+      break;
+    }
+    if(!node.right) {
+      node.right = createTreeNode(key);
+      break;
+    }
+
+    queue.push(node.right);
+    queue.push(node.left);
+  }
+
+  return root;
+
+}
+
+console.log('insert-1 ');
+let root = createTree([10,11,9,7,null,15,8]);
+printTree(root);
+printTree(insert(root, 12))
