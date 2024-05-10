668c4494857bad0ba3ab756c9a8a6c039e2a354c
Arvind Nama <arvind.nu@gmail.com>
2024-05-10 16:22:53 -0700

    [Graphs] Simple problems

--- graphs/helpers/node.ts
+++ graphs/helpers/node.ts
@@ -1,10 +1,11 @@
 
 
+export type Vertex = {[key in string | number]: boolean};
 export type Edge<T extends number | string> = [T, T, number?];
 export type Node<T extends number | string> = { node: T, weight?: number}
 export type AdjacentList<T extends number | string> = {[k in string | number]: Array<Node<T>>};
 
-export type NodeVisited = {[key in number]: boolean};
+export type NodeVisited = {[key in number | string]: boolean};
 
 export interface IGraph<T extends number | string> {
   vertex: number;
@@ -15,13 +16,18 @@ export interface IGraph<T extends number | string> {
 
 export class Graph<T extends number | string> implements IGraph<T> {
   public adjacentList: AdjacentList<T> = {};
+  #vertices: Vertex = {};
 
   public get vertices() {
-    return Object.keys(this.adjacentList);
+    return Object.keys(this.#vertices);
   }
 
   constructor(public vertex: number, edges: Array<Edge<T>>) {
     edges.forEach(([src, dest, weight]) => {
+      // Record vertices 
+      this.#vertices[src] = true;
+      this.#vertices[dest] = true;
+
       this.adjacentList[src] = this.adjacentList[src] || [];
       this.adjacentList[src].push({node: dest, weight});
     });
@@ -34,7 +40,7 @@ export class Graph<T extends number | string> implements IGraph<T> {
   public print() {
     this.vertices.forEach((key) => {
       const edges = this.adjacentList[key];
-      edges.map(({node, weight}) => {
+      edges?.map(({node, weight}) => {
         const connector = weight ? `--(${weight})-->` : `---->`
         console.log(`${key} ${connector} ${node}`)
       })
