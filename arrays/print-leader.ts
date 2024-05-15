e995b8ec4eaf3fd545a905076cf6615f2fd3717e
Arvind Nama <arvind.nu@gmail.com>
2024-05-15 10:20:25 -0700

    [arrays] simple problems - 2

---
+++ arrays/print-leader.ts
@@ -0,0 +1,27 @@
+/**
+ * Write a program to print all the LEADERS in the array. An element is a leader if it is greater than all the elements to its right side. And the rightmost element is always a leader.
+ */
+
+
+function printLeaders(arr: number[]): number[] {
+  let res = [];
+  for (let i = 0; i < arr.length ; i++) {
+    let isLeader = true;
+    for (let j = i + 1 ; j < arr.length; j++) {
+      if(arr[i] < arr[j]){
+        isLeader = false;
+        break;
+      }
+    }
+    if(isLeader) res.push(arr[i])
+  }
+  return res;
+}
+
+let arr = [16, 17, 4, 3, 5, 2]
+console.log('print leaders', arr);
+console.log(printLeaders(arr));
+
+arr = [1, 2, 3, 4, 5, 2]
+console.log('print leaders', arr);
+console.log(printLeaders(arr));
