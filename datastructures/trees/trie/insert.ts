8053f0e6de1e24fce649e02a7c4747763e71e1cb
Arvind Nama <arvind.nu@gmail.com>
2024-07-04 12:12:31 -0700

    [datastructures] trie data structure test sentences

--- datastructures/trees/trie/insert.ts
+++ datastructures/trees/trie/insert.ts
@@ -2,7 +2,7 @@ import { Trie } from '../../../models/trie.models';
 
 console.log('Demonstrate Trie Tree insert operation');
 
-const trie = new Trie('');
+let trie = new Trie('');
 
 console.log('Insert and, ant , dad & do');
 trie.insert('and');
@@ -10,4 +10,15 @@ trie.insert('andrew');
 trie.insert('ant');
 trie.insert('dad');
 trie.insert('do');
+console.log('Print Trie', trie.print());
+
+console.log('Demonstrate Trie Tree insert operation - sentences');
+
+trie = new Trie(' ');
+
+trie.insert('my name is arvind');
+trie.insert('my name is nama');
+trie.insert('how many kids does Arvind have');
+trie.insert('how many jobs does Arvind do');
+trie.insert('how many times does Arvind poop');
 console.log('Print Trie', trie.print());
