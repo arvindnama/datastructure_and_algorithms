e169e98d6eb06ec7ef718209cbe39dc1a7977b6a
Arvind Nama <arvind.nu@gmail.com>
2024-07-07 16:12:45 -0700

    [mockinterview] JS: Fix eslint hack and clean up based on problem statement

--- mock-interviews/js/interview-4/build-tuple.ts
+++ mock-interviews/js/interview-4/build-tuple.ts
@@ -17,40 +17,71 @@
  * console.log(item.multiply(2))
  */
 
+declare global {
+    interface Array<T> {
+        multiply(position: number): T;
+    }
+}
+
 const tuple = (input: string): number[][] => {
     if (!input || !input.trim().length) {
         return [];
     }
 
-    // split based on syntax approach
-
-    // const rowsStrings = input
-    //     .split('),')
-    //     .map((a) => a.replace('(', '').replace(')', ''));
-
-    // const items = rowsStrings.map((rowString) =>
-    //     rowString.split(',').map((i) => parseInt(i, 10))
-    // );
-
-    // regex approach
-    const groups = input.match(/\([^\)]+\)/g);
-
+    const groups = input.match(/\([0-9,]+\)/g);
     if (!groups?.length) {
         return [];
     }
 
     const items = groups
-        .map((g) => g.replace('(', '').replace(')', ''))
-        .map((g) => g.split(',').map((i) => parseInt(i, 10)));
-
-    (items as any).multiply = (position: number): number => {
-        const itemsToMultiply = items.map((row) => row[position - 1]).flat();
-        return itemsToMultiply.reduce((acc, cur) => acc * cur, 1);
-    };
+        .map((row) => /(?<row>[0-9,]+)/.exec(row)?.groups?.row)
+        .map((row) => {
+            return row
+                ? row.split(',').map((i) => parseInt(i, 10))
+                : ([] as number[]);
+        });
 
     return items;
 };
 
-const input = '(1,2,3),(4,5,6),(7,8,9)';
-const items = tuple(input);
-console.log((items as any).multiply(2));
+Array.prototype.multiply = function (position: number): number {
+    if (!this) {
+        return 0;
+    }
+
+    const actualPosInArray = position - 1;
+    const itemsToMultiply = this.map((row) => {
+        return Array.isArray(row) && typeof row[actualPosInArray] === 'number'
+            ? row[actualPosInArray]
+            : 1;
+    }).flat();
+    return itemsToMultiply.reduce((acc, cur) => acc * cur, 1);
+};
+
+console.log(
+    '(1,2,3),(4,5,6),(7,8,9)',
+    tuple('(1,2,3),(4,5,6),(7,8,9)'),
+    'multiply::',
+    tuple('(1,2,3),(4,5,6),(7,8,9)').multiply(2)
+);
+
+console.log(
+    '(1,2,3),(4,5,6),(7,8)',
+    tuple('(1,2,3),(4,5,6),(7,8)'),
+    'multiply::',
+    tuple('(1,2,3),(4,5,6),(7,8)').multiply(3)
+);
+
+console.log(
+    '(1,2,3),(),(7,8)',
+    tuple('(1,2,3),(),(7,8,9)'),
+    'multiply::',
+    tuple('(1,2,3),(),(7,8,9)').multiply(3)
+);
+
+console.log(
+    '(1,2,3),(a),(7,8)',
+    tuple('(1,2,3),(a),(7,8,9)'),
+    'multiply::',
+    tuple('(1,2,3),(a),(7,8,9)').multiply(1)
+);
