d54926a0550ddd4a0e03b383569808a9dd91d7f8
Arvind Nama <arvind.nu@gmail.com>
2024-07-10 21:04:50 -0700

    [mockinterview] leetcode: easy problems

---
+++ mock-interviews/leet-code/remove-duplicates.ts
@@ -0,0 +1,35 @@
+/**
+ * Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+
+ */
+
+import { ListNode, createList, printList } from '../../models/leet-code.models';
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    if (!head) return null;
+
+    let ptr: ListNode | null = head;
+    while (ptr) {
+        const next = ptr.next;
+        if (ptr.val === next?.val) {
+            ptr.next = next.next;
+        } else {
+            ptr = ptr.next;
+        }
+    }
+
+    return head;
+}
+
+let head = createList([1, 1, 2]);
+head = deleteDuplicates(head);
+console.log(printList(head));
+
+head = createList([1, 1, 2, 3, 3]);
+head = deleteDuplicates(head);
+console.log(printList(head));
+
+head = createList([1, 1, 1, 1, 1, 1]);
+head = deleteDuplicates(head);
+console.log(printList(head));
